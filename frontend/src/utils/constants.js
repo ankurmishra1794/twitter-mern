c93a458426c9ae4318bcf6e421cf0809d6d16d22
Mishra, Ankur <ankur.e.mishra@accenture.com>
2024-09-27 11:58:05 +0530

    url updated

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://localhost:4000/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:4000/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-mern-056s.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-mern-056s.onrender.com/api/v1/tweet";
