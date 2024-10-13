0b85693c2f6495b19c5f45b5fca76aad72ac8c65
ankurmishra1794 <50026642+ankurmishra1794@users.noreply.github.com>
2024-10-13 23:47:01 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "http://16.171.177.10:4000/api/v1/user";
-export const TWEET_API_END_POINT = "http://16.171.177.10:4000/api/v1/tweet";
+export const USER_API_END_POINT = "https://ankurmishra.online/api/v1/user";
+export const TWEET_API_END_POINT = "https://ankurmishra.online/api/v1/tweet";
