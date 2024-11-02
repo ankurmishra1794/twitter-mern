d7af0294ed042395bff1c3522a288e42e621f830
Mishra, Ankur <ankur.e.mishra@accenture.com>
2024-11-02 13:00:00 +0530

    updated build

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,9 @@
-export const USER_API_END_POINT = "https://ankurmishra.online/api/v1/user";
-export const TWEET_API_END_POINT = "https://ankurmishra.online/api/v1/tweet";
+export const USER_API_END_POINT = 
+    process.env.NODE_ENV === 'production' 
+        ? "https://ankurmishra.online/api/v1/user" 
+        : 'http://localhost:4000/api/v1/user';
+
+export const TWEET_API_END_POINT = 
+    process.env.NODE_ENV === 'production' 
+        ? "https://ankurmishra.online/api/v1/tweet" 
+        : 'http://localhost:4000/api/v1/tweet';
